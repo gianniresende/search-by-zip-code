e1f1d7413e8807739a017569e241cec808dd8810
gianniresende <gianniresende@gmail.com>
2020-10-28 20:43:11 -0300

    feat: add bootstrap and refactor view index.html

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
-
 function queryCep() {
   var zip_code = document.getElementById("cep").value;
   var url = "https://viacep.com.br/ws/"+ zip_code +"/json/"
   $.ajax({
     url: url,
     type: "GEt",
-    success: function(response) {
-      $("#logradouro").html(response.logradouro);
-      $("#bairro").html(response.bairro);
-      $("#localidade").html(response.localidade);
-      $("#uf").html(response.uf);
+    success: function(response) { 
+      $("#logradouro").html(response.logradouro); 
+      $("#bairro").html(response.bairro); 
+      $("#localidade").html(response.localidade); 
+      $("#uf").html(response.uf);   
+      $("#title_cep").html("CEP " + response.cep); 
       console.log(response);
     }
   })
