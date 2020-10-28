14812b54e54ef41fbebfcfaa450cfb3e9fc9175b
gianniresende <gianniresende@gmail.com>
2020-10-28 19:48:59 -0300

    feat: access address through url

--- js/main.js
+++ js/main.js
@@ -1,9 +1,15 @@
 
 function queryCep() {
+  var zip_code = document.getElementById("cep").value;
+  var url = "https://viacep.com.br/ws/"+ zip_code +"/json/"
   $.ajax({
-    url: "https://viacep.com.br/ws/37206654/json/",
+    url: url,
     type: "GEt",
     success: function(response) {
+      $("#logradouro").html(response.logradouro);
+      $("#bairro").html(response.bairro);
+      $("#localidade").html(response.localidade);
+      $("#uf").html(response.uf);
       console.log(response);
     }
   })
