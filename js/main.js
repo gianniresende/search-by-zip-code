1b04e5bf427a9a7cc668eb28100d3f83af5884f4
gianniresende <gianniresende@gmail.com>
2020-10-28 21:01:02 -0300

    feat: add progress-bar and validate zip code informed

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
 function queryCep() {
+  $(".bar-progress").show();
   var zip_code = document.getElementById("cep").value;
-  var url = "https://viacep.com.br/ws/"+ zip_code +"/json/"
+  var url = "";
+  if (zip_code === ""){
+    $(".bar-progress").hide();
+    alert('Informe um CEP para consulta');
+    return;
+  }else {
+    url = "https://viacep.com.br/ws/"+ zip_code +"/json/"
+  }
   $.ajax({
     url: url,
     type: "GEt",
@@ -9,8 +17,15 @@ function queryCep() {
       $("#bairro").html(response.bairro); 
       $("#localidade").html(response.localidade); 
       $("#uf").html(response.uf);   
-      $("#title_cep").html("CEP " + response.cep); 
-      console.log(response);
+      $("#title_cep").html("CEP " + response.cep);
+      $(".cep").show();
+      $(".bar-progress").hide();
     }
   })
 }
+
+$(function() {
+  $(".cep").hide();
+  $(".bar-progress").hide();
+})
+
